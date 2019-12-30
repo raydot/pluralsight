5cbffe2f6198ca32740da0c9519f71bc1a73c13b
Dave Kanter <moatgoat@hotmail.com>
2019-12-30 15:53:25 -0800

    Through b-a-w-r-a-f Module 6.

--- building-applications-with-react-and-flux/ps-flux/src/components/App.js
+++ building-applications-with-react-and-flux/ps-flux/src/components/App.js
@@ -2,16 +2,18 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
+    if (route === "/courses") return <CoursesPage />;
     if (route === "/about") return <AboutPage />;
     return <HomePage />;
   }
   return (
-    <div class="container-fluid">
+    <div className="container-fluid">
       <Header />
       {getPage()}
     </div>
