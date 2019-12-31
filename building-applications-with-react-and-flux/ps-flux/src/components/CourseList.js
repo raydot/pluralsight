36f1358c01d4c6cc7c020c3a68d9ab334c59e7e3
Dave Kanter <moatgoat@hotmail.com>
2019-12-31 13:05:01 -0800

    End of module 7.

--- building-applications-with-react-and-flux/ps-flux/src/components/CourseList.js
+++ building-applications-with-react-and-flux/ps-flux/src/components/CourseList.js
@@ -1,5 +1,6 @@
 // "Dumb" container
 import React from "react";
+import PropTypes from "prop-types";
 
 // Send array to props and it returns a table.
 function CourseList(props) {
@@ -26,5 +27,22 @@ function CourseList(props) {
     </table>
   );
 }
+
+// Proptypes only checked in development
+CourseList.propTypes = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
+
+// Default to an empty array
+CourseList.defaultProps = {
+  courses: []
+};
 
 export default CourseList;
