2c1707867ce8d35e94ce4a2f407f7ba7e297fe3a
Dave Kanter <moatgoat@hotmail.com>
2020-01-15 12:52:17 -0800

    First commit from desktop, fixed all build issues.

---
+++ isomorphic-react/isomorphic-react/webpack.config.prod.babel.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+
+/**
+ * Production Webpack Config bundles JS, then uglifies it and exports it to the "dist" directory
+ * See Development webpack config for detailed comments
+ */
+module.exports = {
+  entry: ["babel-regenerator-runtime", path.resolve(__dirname, "src")],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  //   optimization: {
+  //       minimize: true,
+  //       minimizer: [new TerserPlugin()],
+  //   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        WEBPACK: true
+      }
+    })
+    /**
+     * Uglifies JS which improves performance
+     * React will throw console warnings if this is not implemented
+     */
+    //new webpack.optimize.UglifyJsPlugin()
+    //var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+    //new UglifyJsPlugin()
+  ],
+  resolve: {
+    extensions: [".js", ".json", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: "babel-loader"
+        },
+        include: path.resolve(__dirname, "src")
+      }
+    ]
+  }
+};
